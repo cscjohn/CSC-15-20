2ff83dc3855622f8a50c583b025b2ca706e46580
cscjohn <158399711+cscjohn@users.noreply.github.com>
2024-09-12 16:02:45 -0700

    Add files via upload

---
+++ Stewie2.java
@@ -0,0 +1,26 @@
+public class Stewie2 {
+    
+    public static void main(String[] args) {
+        
+        printTop();
+        printBottom();
+        printBottom();
+        printBottom();
+        printBottom();
+        printBottom();
+       
+    }
+    
+    public static void printTop(){
+        
+       System.out.println("//////////////////////");
+        
+    }
+    
+    public static void printBottom(){
+        
+        System.out.println("|| Victory is mine! ||");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+        
+    }
+}
