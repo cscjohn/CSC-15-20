156afe6971781751b90f1e16af1c6d191b00d21d
cscjohn <158399711+cscjohn@users.noreply.github.com>
2024-09-12 16:11:42 -0700

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,13 @@
+public class Hello
+{
+   public static void main(String[] args)
+   {
+      System.out.print("Hello Class");
+      System.out.println();
+      System.out.print("John Vue");
+      System.out.println();
+      System.out.print("1 2 3 4 5 6 7 8 9 10");
+      //mailing address: John Vue, 7679 Rotherton Way, Sacramento. CA, 95823
+      System.out.println();
+   }
+}
